2dca7807369d9605117de5257a3ee728336d4e11
Gimmyhat <nikitchenko.as@gmail.com>
2023-04-17 10:37:40 +0800

    Dashboard, edit & delete jobs , etc

--- jobapp/forms.py
+++ jobapp/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 
-from jobapp.models import Job
+from jobapp.models import Job, Applicant, BookmarkJob
 
 
 class JobForm(forms.ModelForm):
@@ -79,6 +79,94 @@ class JobForm(forms.ModelForm):
 
     def save(self, commit=True):
         job = super(JobForm, self).save(commit=False)
+        if commit:
+            job.save()
+        return job
+
+
+class JobApplyForm(forms.ModelForm):
+    class Meta:
+        model = Applicant
+        fields = ['job']
+
+
+class JobBookmarkForm(forms.ModelForm):
+    class Meta:
+        model = BookmarkJob
+        fields = ['job']
+
+
+class JobEditForm(forms.ModelForm):
+
+    def __init__(self, *args, **kwargs):
+        forms.ModelForm.__init__(self, *args, **kwargs)
+        self.fields['title'].label = "Наименование вакансии :"
+        self.fields['location'].label = "Локация вакансии :"
+        self.fields['salary'].label = "Зарплата :"
+        self.fields['description'].label = "Описание вакансии :"
+        # self.fields['tags'].label = "Теги :"
+        self.fields['company_name'].label = "Наименование компании :"
+        self.fields['url'].label = "Адрес сайта :"
+
+        self.fields['title'].widget.attrs.update(
+            {
+                'placeholder': 'например: Программист',
+            }
+        )
+        self.fields['location'].widget.attrs.update(
+            {
+                'placeholder': 'например: Москва',
+            }
+        )
+        self.fields['salary'].widget.attrs.update(
+            {
+                'placeholder': '$800 - $1200',
+            }
+        )
+
+        self.fields['company_name'].widget.attrs.update(
+            {
+                'placeholder': 'например: ООО Тритон',
+            }
+        )
+        self.fields['url'].widget.attrs.update(
+            {
+                'placeholder': 'https://example.com',
+            }
+        )
+
+    class Meta:
+        model = Job
+
+        fields = [
+            "title",
+            "location",
+            "job_type",
+            "category",
+            "salary",
+            "description",
+            "company_name",
+            "company_description",
+            "url"
+        ]
+
+    def clean_job_type(self):
+        job_type = self.cleaned_data.get('job_type')
+
+        if not job_type:
+            raise forms.ValidationError("Job Type is required")
+        return job_type
+
+    def clean_category(self):
+        category = self.cleaned_data.get('category')
+
+        if not category:
+            raise forms.ValidationError("Category is required")
+        return category
+
+    def save(self, commit=True):
+        job = super(JobEditForm, self).save(commit=False)
+
         if commit:
             job.save()
         return job
--- jobapp/urls.py
+++ jobapp/urls.py
@@ -6,6 +6,7 @@ app_name = "jobapp"
 
 urlpatterns = [
     path('', RedirectView.as_view(url="news/")),
+    path('jobs/', views.job_list_view, name='job-list'),
     path('news/', views.NewsPageView.as_view(), name='news'),
     #path('news/detail/<int:id>/', ...),
     path('about/', views.AboutPageView.as_view(), name='about'),
@@ -13,4 +14,11 @@ urlpatterns = [
     path('', views.home_view, name='home'),
     path('jobs/create/', views.create_job_view, name='create-job'),
     path('job/<int:id>/', views.single_job_view, name='single-job'),
+    path('dashboard/', views.dashboard_view, name='dashboard'),
+    path('dashboard/employer/job/<int:id>/applicants/', views.all_applicants_view, name='applicants'),
+    path('dashboard/employer/job/edit/<int:id>', views.job_edit_view, name='edit-job'),
+    path('dashboard/employer/applicant/<int:id>/', views.applicant_details_view, name='applicant-details'),
+    path('dashboard/employer/close/<int:id>/', views.make_complete_job_view, name='complete'),
+    path('dashboard/employer/delete/<int:id>/', views.delete_job_view, name='delete'),
+    path('dashboard/employer/delete/<int:id>/', views.delete_job_view, name='delete'),
 ]
